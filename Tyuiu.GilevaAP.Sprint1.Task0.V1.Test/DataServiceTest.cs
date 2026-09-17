36d38091cb9e69df1b1092ec9725e5e3121155d8
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-17 17:54:56 +0500

    task0

---
+++ Tyuiu.GilevaAP.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GilevaAP.Sprint1.Task0.V1.Lib;
+namespace Tyuiu.GilevaAP.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExperession()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-6, res);
+        }
+    }
+}
